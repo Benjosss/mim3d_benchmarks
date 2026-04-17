195144a31a75d247b4b5191819146c54b65074e0
Benjosss <b55.lallement@gmail.com>
2026-04-17 10:07:04 +0200

    [FIX] : Updated camera spawn point

--- main.js
+++ main.js
@@ -79,6 +79,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x1a1a2e);
 
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 10000);
+camera.position.set(...CONFIG.spawnPoint);
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
